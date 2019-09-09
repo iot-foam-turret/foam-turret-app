5f0539c4ac0ca6f06fcb9dbe1aa93747a9e55b27
Laura Tuck <laura.tuck@slalom.com>
2019-09-09 15:20:03 -0700

    Added pubsub stuff

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { PubSub } from "aws-amplify";
 
 export default function Home() {
+  useEffect(() => {
+    PubSub.subscribe('$aws/things/Pi-Turret/shadow/update').subscribe({
+      next: (data: any) => console.log('Message received', data),
+      error: (error: any) => console.error(error),
+      close: () => console.log('Done'),
+    });
+  }, []);
+
   return <div>Home Page</div>;
 }
