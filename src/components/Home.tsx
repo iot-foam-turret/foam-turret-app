a08c57470cfb31f2ebc501f5c93c2201e65c7e0e
Corwin Marsh <corwinm@users.noreply.github.com>
2019-09-09 19:43:31 -0700

    I think this is working so I'm going to commit it

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { PubSub } from "aws-amplify";
 
-export default function Home() {
+const piTurretUpdateTopic = "$aws/things/Pi-Turret/shadow/update";
+
+type iotTurretState = {
+  pitch: Number;
+  yaw: Number;
+};
+
+function useIoT(): [iotTurretState, (desiredState: iotTurretState) => void] {
+  const [turretState, setTurretState] = useState<iotTurretState>({
+    pitch: 0,
+    yaw: 0
+  });
   useEffect(() => {
-    PubSub.subscribe('$aws/things/Pi-Turret/shadow/update').subscribe({
-      next: (data: any) => console.log('Message received', data),
+    // Auth.currentCredentials().then((info) => {
+    //   console.log(info.identityId)
+    // });
+
+    const sub = PubSub.subscribe(piTurretUpdateTopic).subscribe({
+      next: (data: any) => {
+        console.log("Message received", data);
+        if (data && data.value && data.value.state && data.value.state.reported) {
+          setTurretState(data.value.state.reported);
+        }
+      },
       error: (error: any) => console.error(error),
-      close: () => console.log('Done'),
+      close: () => console.log("Done")
     });
+    return () => sub.unsubscribe();
   }, []);
 
-  return <div>Home Page</div>;
+  async function setDesiredState(newState: iotTurretState) {
+    await PubSub.publish(piTurretUpdateTopic, {
+      state: {
+        desired: newState
+      }
+    });
+  }
+
+  return [turretState, setDesiredState];
+}
+export default function Home() {
+  const [turretState, setTurretState] = useIoT();
+  return (
+    <>
+      <h2>Home Page</h2>
+      <div>
+        <button onClick={() => setTurretState({ pitch: 10, yaw: 10 })}>
+          Test
+        </button>
+        <div>{JSON.stringify(turretState)}</div>
+      </div>
+    </>
+  );
 }
