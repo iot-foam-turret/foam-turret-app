1bcacc51672290012640369545aea924fac437b1
Christopher Moore <christopherkmoore1@gmail.com>
2019-09-14 10:17:51 -0700

    adds some shit

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import History from "./History";
--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import Amplify, { Storage } from 'aws-amplify';
 
-export default function Upload() {
-  return <div>Upload Picture</div>;
+class Upload extends React.Component { 
+    onChange(e: any) {
+        const file = e.target.files[0];
+        Storage.put('example.png', file, {
+            contentType: 'image/png'
+        })
+        .then (result => console.log(result))
+        .catch(err => console.log(err));
+    }
+  
+    render() {
+        return (
+            <input
+                type="file" accept='image/png'
+                onChange={(e) => this.onChange(e)}
+            />
+        )
+    }
 }
+export default Upload;
+// }
+// export default function Upload() {
+//     Storage.put()
+
+//   return <div>Upload Picture</div>;
+// }
